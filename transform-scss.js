819a4a7bce09cfb6ee97d17fc8dea3b9fd434a6b
g_e_out <g_e@daum.net>
2024-06-28 12:58:56 +0900

    뎁스분리 추가

--- transform-scss.js
+++ transform-scss.js
@@ -1,18 +1,6 @@
 // node
 const StyleDictionary = require("style-dictionary");
 
-// transformName
-function transformName(prop, suffix) {
-  if (suffix) {
-    const path = prop.path;
-    path[0] = suffix;
-    return path.join("-");
-  } else {
-    return prop.path.slice(1).join("-"); // 첫 번째 요소 제외
-    // return prop.path.slice(-1).join("-"); // 마지막
-    // return prop.path.slice(-2).join("-"); // 끝에서 두 번째 
-  }
-}
 
 // preset
 // const preset = `@charset "utf-8";\n\n//prefix\n$prefix: krds- !default;\n$box-shadow-base: 2px 2px 2px 0px rgba(0, 0, 0, 0.15) !default;\n\n`;
@@ -35,6 +23,21 @@ $box-shadow-base: 2px 2px 2px 0px rgba(0, 0, 0, 0.15) !default;
 
 `;
 
+// transformName
+function transformName(prop, slice, suffix) {
+  if (suffix) {
+    // const path = prop.path;
+    // path[0] = suffix;
+    // return path.join("-");
+    const path = prop.path.slice(slice).join("-");
+    return `${suffix}-${path}`;
+  } else {
+    // return prop.path.slice(1).join("-"); // 첫 번째 요소 제외
+    // return prop.path.slice(-1).join("-"); // 마지막
+    // return prop.path.slice(-2).join("-"); // 끝에서 두 번째 
+    return prop.path.slice(slice).join("-");
+  }
+}
 
 // custom
 StyleDictionary.registerFormat({
@@ -44,11 +47,11 @@ StyleDictionary.registerFormat({
       // const type = prop.type;
       const type = prop.path[0];
       // const type = prop.description;
+      const name = transformName(prop, 1, type);
+
       if (!acc[type]) {
         acc[type] = [];
       }
-      const name = transformName(prop, type);
-      // acc[type].push(`  ${type}-${name}: ${prop.value},`);
       acc[type].push(`  ${name}: ${prop.value},`);
       return acc;
     }, {});
@@ -60,37 +63,148 @@ StyleDictionary.registerFormat({
       .join("\n\n");
 
     // return scssContent;
-    return preset + scssContent;
+    return scssContent;
   }
 });
 
-// StyleDictionary.registerFormat({
-//   name: "custom/scss-format2",
-//   formatter: function(dictionary) {
-//     const typeGroups = dictionary.allProperties.reduce((acc, prop) => {
-//       // const type = prop.type;
-//       // const type = prop.path[0];
-//       const type = prop.description;
-//       if (type !== undefined) { 
-//         if (!acc[type]) {
-//           acc[type] = [];
-//         }
-//         const name = transformName(prop);
-//         acc[type].push(`  ${name}: ${prop.value},`);
-//       }
-//       return acc;
-//     }, {});
 
-//     const scssContent = Object.entries(typeGroups)
-//       .map(([type, tokens]) => {
-//         return `//${type}\n$${type}: (\n${tokens.join("\n")}\n)!default;`;
-//       })
-//       .join("\n\n");
+// custom cti
+StyleDictionary.registerFormat({
+  name: "custom/scss-format2",
+  formatter: function(dictionary) {
+    const typeGroups = dictionary.allProperties.reduce((acc, prop) => {
+      // cti : category, type, item, subitem, state
+      const { category, type, item } = prop.attributes;
+      const name = transformName(prop, -1);
 
-//     // return scssContent;
-//     return preset + scssContent;
-//   }
-// });
+      // if (!acc[category]) {
+      //   acc[category] = {};
+      // }
+      // if (!acc[category][type]) {
+      //   acc[category][type] = {};
+      // }
+      // if (!acc[category][type][item]) {
+      //   acc[category][type][item] = [];
+      // }
+
+      acc[category] ??= {};
+      acc[category][type] ??= {};
+      acc[category][type][item] ??= [];
+      acc[category][type][item].push(`      ${name}: ${prop.value},`);
+      return acc;
+    }, {});
+
+    const scssContent = Object.entries(typeGroups)
+      .map(([category, types]) => {
+        const typeContent = Object.entries(types)
+          .map(([type, items]) => {
+            const itemContent = Object.entries(items)
+              .map(([item, tokens]) => {
+                return `    ${item}: (\n${tokens.join("\n")}\n    ),`;
+              })
+              .join("\n");
+            return `  ${type}: (\n${itemContent}\n  ),`;
+          })
+          .join("\n");
+        return `$${category}: (\n${typeContent}\n)!default;`;
+      })
+      .join("\n\n");
+
+    return scssContent;
+  }
+});
+
+
+// custom cti2
+StyleDictionary.registerFormat({
+  name: "custom/scss-format3",
+  formatter: function(dictionary) {
+    const typeGroups = dictionary.allProperties.reduce((acc, prop) => {
+      const { category, type } = prop.attributes;
+      const name = transformName(prop, -2);
+
+      acc[category] ??= {};
+      acc[category][type] ??= [];
+      acc[category][type].push(`    ${name}: ${prop.value},`);
+      
+      return acc;
+    }, {});
+
+    const scssContent = Object.entries(typeGroups)
+      .map(([category, types]) => {
+        const typeContent = Object.entries(types)
+          .map(([type, tokens]) => {
+            return `  ${type}: (\n${tokens.join("\n")}\n  ),`;
+          })
+          .join("\n");
+        return `$${category}: (\n${typeContent}\n)!default;`;
+      })
+      .join("\n\n");
+
+    return scssContent;
+  }
+});
+
+// custom path
+StyleDictionary.registerFormat({
+  name: "custom/scss-format4",
+  formatter: function(dictionary) {
+    const typeGroups = dictionary.allProperties.reduce((acc, prop) => {
+      const path0 = prop.path[0];
+      const path1 = prop.path[1];
+      const name = transformName(prop, -2, path1);
+
+      acc[path0] ??= {};
+      acc[path0][path1] ??= [];
+      acc[path0][path1].push(`    ${name}: ${prop.value},`);
+      
+      return acc;
+    }, {});
+
+    const scssContent = Object.entries(typeGroups)
+      .map(([path0, types]) => {
+        const typeContent = Object.entries(types)
+          .map(([path1, tokens]) => {
+            return `  ${path1}: (\n${tokens.join("\n")}\n  ),`;
+          })
+          .join("\n");
+        return `$${path0}: (\n${typeContent}\n)!default;`;
+      })
+      .join("\n\n");
+
+    return scssContent;
+  }
+});
+
+
+
+
+// registerFilter
+StyleDictionary.registerFilter({
+  name: 'isColor',
+  matcher: function(prop) {
+    return prop.type === "color";
+  }
+})
+StyleDictionary.registerFilter({
+  name: 'isString',
+  matcher: function(prop) {
+    return prop.type === "string";
+  }
+})
+StyleDictionary.registerFilter({
+  name: 'isDimension',
+  matcher: function(prop) {
+    return prop.type === "dimension";
+  }
+})
+StyleDictionary.registerFilter({
+  name: 'isNumber',
+  matcher: function(prop) {
+    return prop.type === "number";
+  }
+})
+
 
 // export
 module.exports = {
@@ -98,17 +212,18 @@ module.exports = {
     // "./app/src/assets/tokens/**/*",
     // "./app/src/assets/tokens/token.json",
     // "./app/src/assets/tokens/design-token.json"
-    "./app/src/assets/tokens/local-variables.tokens.json",
-    "./app/src/assets/tokens/style-typography.tokens.json"
+    // "./app/src/assets/tokens/local-variables.tokens.json",
+    // "./app/src/assets/tokens/style-typography.tokens.json"
+    "./app/src/assets/tokens/test.tokens.json"
   ],
   platforms: {
-    scss1: {
+    scss: {
       transformGroup: "scss",
       buildPath: "./app/src/assets/scss/abstracts/",
       files: [
         {
           destination: "_variables.scss",
-          format: "custom/scss-format",
+          format: "custom/scss-format4",
         },
       ],
     },
@@ -117,8 +232,42 @@ module.exports = {
     //   buildPath: "./app/src/assets/scss/abstracts/",
     //   files: [
     //     {
-    //       destination: "_variables-test.scss",
-    //       format: "custom/scss-format2",
+    //       destination: "_variables2.scss",
+    //       format: "custom/scss-format",
+    //       filter: "isColor",
+    //     },
+    //   ],
+    // },
+    // scss3: {
+    //   transformGroup: "scss",
+    //   buildPath: "./app/src/assets/scss/abstracts/",
+    //   files: [
+    //     {
+    //       destination: "_variables3.scss",
+    //       format: "custom/scss-format",
+    //       filter: "isString",
+    //     },
+    //   ],
+    // },
+    // scss4: {
+    //   transformGroup: "scss",
+    //   buildPath: "./app/src/assets/scss/abstracts/",
+    //   files: [
+    //     {
+    //       destination: "_variables4.scss",
+    //       format: "custom/scss-format",
+    //       filter: "isDimension",
+    //     },
+    //   ],
+    // },
+    // scss5: {
+    //   transformGroup: "scss",
+    //   buildPath: "./app/src/assets/scss/abstracts/",
+    //   files: [
+    //     {
+    //       destination: "_variables5.scss",
+    //       format: "custom/scss-format",
+    //       filter: "isNumber",
     //     },
     //   ],
     // },
